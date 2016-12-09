9695622668a800e4d528bbfdf6985e56cd8ce046
Bruce  Powell <powelb2@bobby.CS.UNLV.EDU>
2016-12-08 19:57:07 -0800

    learning git, commiting again

--- ex03file.cpp
+++
@@ -1,9 +0,0 @@
-//your name, lecture section #, lab section #, exercise #
-#include <iostream>
-using namespace std;
-
-int main()
-{
-  cout << "Exercise #3" << endl;
-  return 0;
-}
--- exercise3.cpp
+++ exercise3.cpp
@@ -1,17 +1,9 @@
-//Bruce Powell, lecture section #1003, lab section 1009, exercise 3
-
+//your name, lecture section #, lab section #, exercise #
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  cout << "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB" << endl;
-  cout << "Name: Bruce Powell" << endl;  
   cout << "Exercise #3" << endl;
-  cout << "Lecture Section# 1003" << endl;
-  cout << "Lab Section# 1009" << endl;
-  cout << " " << endl;
-  cout << "Email: powelb2@unlv.nevada.edu" << endl;
-  cout << "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP" << endl;
   return 0;
 }
